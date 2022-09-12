a36cf2a3571623b0ca08bc593a3762667b7f1c17
Fabio Sousa <clubeteczagem@gmail.com>
2022-09-12 06:58:07 -0300

    inserindo componente formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardsComponent } from './cards/cards.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardsComponent
+    CardsComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
